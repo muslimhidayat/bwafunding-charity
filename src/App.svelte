24eb2fee1e7ca3cd8c6b661143e14a593173923d
Muslim Hidayat <muslimhidayat@gmail.com>
2020-09-11 20:34:52 +0800

    Convert HTML to svelte components

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,5 @@
 <script>
-  export let name;
+  import Home from "./pages/Home.svelte";
 </script>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
-
-<main>
-  <h1>I loove {name}!</h1>
-  <p>from karaeng pattingaloang..</p>
-</main>
-
-<footer>&copy culling 2020</footer>
+<Home />
---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+  charities: [
+    {
+      id: 1,
+      title: "first Charity Project",
+      category: "money",
+      thumbnail: "./assets/images/causes/causes_4.png",
+      pledged: 99000,
+      target: 100000,
+      date_end: +new Date("10 november 2020"),
+      profile_photo: "./assets/images/avatar/avatar_8.jpg",
+      profile_name: "Karaeng Pattingaloang",
+      no_pledges: 0,
+    },
+  ],
+};
