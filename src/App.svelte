b570be3cde4ff8f07ec0c5d030fe304e7f693917
Muslim Hidayat <muslimhidayat@gmail.com>
2020-09-13 00:16:33 +0800

    Add netlify config file

--- src/App.svelte
+++ src/App.svelte
@@ -6,12 +6,14 @@
   import Donation from "./pages/Donation.svelte";
   import NotFound from "./pages/NotFound.svelte";
   import Succes from "./pages/Succes.svelte";
+  import Failure from "./pages/Failure.svelte";
 
   let page, params;
   router("/", () => (page = Home));
   router("/about", () => (page = About));
   router("/contact", () => (page = Contact));
   router("/succes", () => (page = Succes));
+  router("/error", () => (page = Failure));
   router(
     "/donation/:id",
     (ctx, next) => {
